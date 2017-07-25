a2cca1d0b77f2c3c5063046a3160490feebe65bd
Florêncio Vieira Filho <florenciovieira@gmail.com>
2017-07-25 19:05:55 -0300

    Exclusão de test

--- app/src/main/java/turma_android/com/br/appnotificacao/MainActivity.java
+++ app/src/main/java/turma_android/com/br/appnotificacao/MainActivity.java
@@ -1,10 +1,13 @@
 package turma_android.com.br.appnotificacao;
 
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private int id = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +16,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void criarNotificacao(View view) {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this);
+
+        builder.setContentTitle("Título");
+        builder.setContentText("Mensagem");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+
+        NotificationManagerCompat manager =
+                NotificationManagerCompat.from(this);
+
+        manager.notify(id, builder.build());
     }
 
     public void cancelarNotificacao(View view) {
+        NotificationManagerCompat manager =
+                NotificationManagerCompat.from(this);
+
+        manager.cancel(id);
     }
 }
