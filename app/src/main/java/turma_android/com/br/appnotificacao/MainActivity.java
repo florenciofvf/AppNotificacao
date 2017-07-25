23bb9a59cc354b738c4c001cc1091a96abbb001d
Florêncio Vieira Filho <florenciovieira@gmail.com>
2017-07-25 18:49:42 -0300

    Initial commit

---
+++ app/src/main/java/turma_android/com/br/appnotificacao/MainActivity.java
@@ -0,0 +1,20 @@
+package turma_android.com.br.appnotificacao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+    public void criarNotificacao(View view) {
+    }
+
+    public void cancelarNotificacao(View view) {
+    }
+}
